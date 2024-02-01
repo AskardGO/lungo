8655fb09547f9016c8b9f6881784d7f482e3b07c
AskardGO <askard@zohomail.com>
2024-02-01 02:40:29 +0200

    FINAL UPDATE OF HEADER ANIMATION!

--- src/components/Navigation/Header/Header.tsx
+++ src/components/Navigation/Header/Header.tsx
@@ -1,58 +1,111 @@
-import React, { useEffect, useState } from "react";
-import styles from './Header.module.scss'
+import React, {useEffect, useLayoutEffect, useRef} from "react";
+import styles from './Header.module.scss';
 import { NavigationItems } from "./NavigationItems/NavigationItems";
-import {NavLink, useLocation} from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Translations } from "./Translations/Translations.tsx";
-
 import GoHomeButton from "./GoHomeButton/GoHomeButton.tsx";
-import {a, useResize, useSpring} from "@react-spring/web";
+import { a, useSpring, useSpringRef } from "@react-spring/web";
 
 export const Header: React.FC = () => {
+    const textRef = useSpringRef();
+    const textBoxRef = useSpringRef();
+    const { pathname } = useLocation();
+    const nav = useNavigate();
 
-    const [isHome, setIsHome] = useState(false)
+    const isHome = pathname === '/';
+    const isAnimationPlayed = useRef(false);
+
+    const [titleBoxProps, titleBoxApi] = useSpring(() => ({
+        ref: textBoxRef,
+        reverse: !isHome,
+        config: { duration: 300 },
+    }));
+
     const [titleProps, titleApi] = useSpring(() => ({
-        to: {
-            opacity: 1,
-            scale: 1,
-            height: 60
-        },
-        from: {
-            opacity: 0,
-            scale: 0.9,
-            height: 0
-        },
-        reverse: isHome,
-        config: {
-            duration: 600,
-        },
-    }), [isHome]);
-    const {pathname} = useLocation();
+        ref: textRef,
+        reverse: !isHome,
+        config: { duration: 200 },
+    }));
+
+    const handleEscape = () => {
+        if (isHome) {
+            nav("/");
+        }
+    };
 
     useEffect(() => {
-        setIsHome(pathname === '/')
-    }, [pathname])
+        document.addEventListener("keydown", handleEscape, false);
+        return () => document.removeEventListener("keydown", handleEscape, false);
+    }, [nav]);
 
-    return(
+    useLayoutEffect(() => {
+        // Setup animation
+        titleApi.start({
+            to: {
+                opacity: isHome ? 0 : 1,
+                scale: isHome ? 0.9 : 1,
+            },
+            config: { duration: 200 },
+        });
+
+        titleBoxApi.start({
+            to: {
+                height: isHome ? 0 : 60,
+            },
+            config: { duration: 200 },
+        });
+    }, []);
+
+    useEffect(() => {
+
+        if (!isAnimationPlayed.current) {
+            isAnimationPlayed.current = true;
+            return;
+        }
+
+        setTimeout(() => {
+            titleApi.start({
+                to: {
+                    opacity: !isHome ? 1 : 0,
+                    scale: !isHome ? 1 : 0.9,
+                },
+                config: { duration: 200 },
+            });
+        }, isHome ? 200 : 300);
+
+        setTimeout(() => {
+            titleBoxApi.start({
+                to: {
+                    height: isHome ? 0 : 60,
+                },
+                config: { duration: 200 },
+            });
+        }, isHome ? 300 : 200);
+
+    }, [isHome, titleProps, titleBoxProps]);
+
+    return (
         <div className={styles.header}>
-            <a.div style={titleProps} className={styles.headerTitle}>
+            <a.div style={titleBoxProps} className={`${styles.headerTitle} ${!isHome && styles.headerTitle_full}`}>
                 <NavLink to={'/'}>
-                    <h3>
-                        lunga la notte
-                    </h3>
+                    <a.div style={titleProps}>
+                        <h3>
+                            lunga la notte
+                        </h3>
+                    </a.div>
                 </NavLink>
             </a.div>
-            <div className={styles.container} style={{margin: `${isHome?.2:1}rem 0`}}>
-                <div style={{position: 'relative'}} className={styles.containerAdditional}>
-                    <GoHomeButton isHome={isHome}/>
+            <div className={styles.container} style={{ margin: isHome ? '0.2rem 0' : '1rem 0' }}>
+                <div style={{ position: 'relative' }} className={styles.containerAdditional}>
+                    <GoHomeButton isHome={isHome} />
                 </div>
-                <div className={styles.containerNav} style={{margin: isHome ? 'inherit' : 0}}>
-                    <NavigationItems selectedPath={pathname} isHome={isHome}/>
+                <div className={styles.containerNav} style={{ margin: isHome ? 'inherit' : 0 }}>
+                    <NavigationItems selectedPath={pathname} isHome={isHome} />
                 </div>
                 <div className={`${styles.containerAdditional} ${styles.containerTraslation}`}>
-                    <Translations isHome={isHome}/>
+                    <Translations isHome={isHome} />
                 </div>
             </div>
         </div>
-
-    )
-}
+    );
+};
